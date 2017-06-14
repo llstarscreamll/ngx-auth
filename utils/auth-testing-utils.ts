48dda1450531da786c5be8e17624838ad1580125
Johan Alvarez <llstarscreamll@hotmail.com>
2017-06-13 21:03:09 -0500

    add testings components and directives to testing module to prevent errors on ng build --prod

--- utils/auth-testing-utils.ts
+++ utils/auth-testing-utils.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { Directive, Input } from "@angular/core";
+import { NgModule, Directive, Input } from '@angular/core';
 
 @Directive({ selector: '[userCanAny]' })
 export class UserCanAnyDumyDirective { @Input() userCanAny: any; }
@@ -7,3 +7,11 @@ export class UserCanAnyDumyDirective { @Input() userCanAny: any; }
 export const AUTH_TESTING_COMPONENTS = [
   UserCanAnyDumyDirective
 ];
+
+@NgModule({
+  imports: [],
+  exports: [...AUTH_TESTING_COMPONENTS],
+  declarations: [...AUTH_TESTING_COMPONENTS],
+})
+export class AuthTestingModuleModule { }
+

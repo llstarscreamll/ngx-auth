dafe5726893a7f254a1cc755e1f0755d8e2c1b4a
Johan Alvarez <llstarscreamll@hotmail.com>
2017-06-16 00:59:45 -0500

    add english language translations

--- auth.module.ts
+++ auth.module.ts
@@ -9,6 +9,7 @@ import { AuthEffects } from './effects/auth.effects';
 import { CoreSharedModule } from 'app/core/core.shared.module';
 
 import { ES } from './translations/es';
+import { EN } from './translations/en';
 import { COMPONENTS } from './components';
 import { AuthRoutingModule } from './auth-routing.module';
 
@@ -26,6 +27,7 @@ export class AuthModule {
 
   public constructor(translate: TranslateService) {
     translate.setTranslation('es', ES, true);
+    translate.setTranslation('en', EN, true);
   }
 
 }
---
+++ translations/en.ts
@@ -0,0 +1,21 @@
+export const EN = {
+	'AUTH': {
+		// Titles
+		'Login': 'Login',
+		'Register': 'Sign up',
+		// Links
+		'Go to Lading Page': 'Go to home',
+		'Already have an account?': 'Already have an account?',
+		// form fields
+		'Full name': 'Full name',
+		'Email': 'Email',
+		'Password': 'Password',
+		'Retype password': 'Confirm password',
+		'I agree to the terms': 'I agree the <a href="{{ link }}">terms</a>',
+		// buttons
+		'Sign In': 'Login',
+		'Create Account': 'Create Acount',
+		// Validation Messages
+		'required field': 'This field is required.'
+	}
+}
